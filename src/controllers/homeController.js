cfc2230865fdd8b88f3c4955eaaf150b551fb445
Đỗ Hoàng Hải <21012870@st.phenikaa-uni.edu.vn>
2024-03-23 17:59:38 +0700

    #5.1 show list user from database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,16 +4,16 @@ const handleHelloworld = (req,res)=>{
   return res.render("home.ejs")
 }
 
-const handleUserPage = (req,res)=>{
-  return res.render("user.ejs")
+const handleUserPage = async(req,res)=>{
+  let userList = await userService.getUsers();
+  return res.render("user.ejs",{userList})
 }
 
 const handleCreateNewUser = (req,res)=>{
   let email = req.body.email;
   let password = req.body.password;
   let username = req.body.username;
-  // userService.createNewUser(email,password,username)
-  userService.getUsers()
+  userService.createNewUser(email,password,username)
   return res.send("handle create new user")
 } 
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,39 +1,49 @@
 import bcrypt from 'bcryptjs';
 const salt = bcrypt.genSaltSync(10);
-import mysql from "mysql2"
+// import mysql from 'mysql2' 
+import mysql from 'mysql2/promise';
+import bluebird from 'bluebird';
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'jwt',
-});
 const hashPassword = (userPassword)=>{
   let hashPassword = bcrypt.hashSync(userPassword, salt); 
   return hashPassword;
 }
 
-const createNewUser = (email,password,username)=>{
+const createNewUser = async (email, password, username) => {
   let hashPass = hashPassword(password);
-  connection.query(
-    `INSERT INTO users (email,password,username) VALUES ("${email}","${hashPass}","${username}")`,
-    function (err, results, fields) {
-      if(err){
-        console.log(err)
-      }
-    }
-  );
-}
-const getUsers =()=>{
+  try {
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      database: 'jwt',
+    });
+
+    await connection.execute(
+      `INSERT INTO users (email,password,username) VALUES (?, ?, ?)`,
+      [email, hashPass, username]
+    );
+
+    await connection.end(); // Close the connection
+  } catch (e) {
+    console.log(">> check e: ", e);
+  }
+};
+
+const getUsers =async()=>{
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'jwt',
+    Promise: bluebird,
+  });
+  
   let users=[];
-  connection.query(
-    `SELECT * from users `,
-    function (err, results, fields) {
-      if(err){
-        console.log(err)
-      }
-      console.log(">>> result: ",results)
-    }
-  );
+  try{
+    const [rows,fields] = await connection.execute('SELECT * from users')
+    return rows
+  }catch(e){
+    console.log("check error: ",e)
+  }
 }
 module.exports={
   createNewUser,getUsers
